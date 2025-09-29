9d180cbd12e3883601b198706ddbb46afe9a9c48
jselbyaao <jselby@aaortho.org>
2025-09-28 22:17:18 -0500

    Update streamlit_app.py
    Moved Request package and added .json

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -56,3 +52,6 @@ if ingredients_list:
         #st.success('Your Smoothie is ordered!', icon="✅")
         st.success('Your Smoothie is ordered '+ name_on_order +'!', icon="✅")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response).json
